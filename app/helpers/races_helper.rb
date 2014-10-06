debeb477a78c7e145bef04a85e3ee8e36004881a
Ulisses <anizark@gmail.com>
2014-10-06 20:40:21 -0300

    Change the tarit class for races helper
    Was a bad idea to move format stuff to a model, including that
    we don't to merge now the race and the subrace stuff. Is better
    show the contents separated to become easier for user see
    the difference.

--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -1,2 +1,30 @@
 module RacesHelper
+  ABILITY_MAP = {
+    str: 'Strength',
+    dex: 'Dexterity',
+    con: 'Constitution',
+    int: 'Inteligence',
+    wis: 'Wisdom',
+    cha: 'Charisma'
+  }
+
+  def traits_for(race)
+    [
+      format_abilities_scores(race[:ability_scores]),
+      format_age_range(race[:age_range])
+    ].select(&:present?)
+  end
+
+  def format_abilities_scores(ability_scores)
+    safe_ability_scores = Hash(ability_scores)
+    safe_ability_scores.map do |ability, score|
+      "#{ABILITY_MAP[ability]} + #{score}"
+    end.join(' | ')
+  end
+
+  def format_age_range(age_range)
+    if age_range
+      "Became mature when reach the age of #{age_range.min}. They can live about #{age_range.max} years."
+    end
+  end
 end
---
+++ test/helpers/races_helper_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class RacesHelperTest < ActionView::TestCase
+  include RacesHelper
+
+  test 'should format the abilities score' do
+    traits = traits_for(ability_scores: { con: 1, dex: 2, str: 3 })
+
+    assert_equal(['Constitution + 1 | Dexterity + 2 | Strength + 3'], traits)
+  end
+
+  test 'should format the age range' do
+    traits = traits_for(age_range: (1..10))
+
+    assert_equal(['Became mature when reach the age of 1. They can live about 10 years.'], traits)
+  end
+end
