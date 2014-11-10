ff6c70d47207f8ea9673083aac503fd88ae0f250
Ulisses <anizark@gmail.com>
2014-11-09 22:17:42 -0200

    Add hp information for 1st level.

--- app/helpers/character_classes_helper.rb
+++ app/helpers/character_classes_helper.rb
@@ -1,13 +1,30 @@
 module CharacterClassesHelper
+  MAX_DICE_VALUE = {
+    :d12 => 12,
+    :d10 => 10,
+    :d8 => 8,
+    :d6 => 6,
+    :d4 => 4,
+    :d3 => 3,
+    :d2 => 2,
+  }
+
   def class_features_for(character_class)
     [
-      label_and_value('Hit Dice', format_hit_dice(character_class[:hit_dice]))
+      label_and_value('Hit Dice', format_hit_dice(character_class[:hit_dice])),
+      label_and_value('Hit Points at 1st Level', format_hp_first_level(character_class[:hit_dice]))
     ].select(&:present?)
   end
 
   def format_hit_dice(hit_dice)
     if hit_dice
       "1#{hit_dice} per level"
+    end
+  end
+
+  def format_hp_first_level(hit_dice)
+    if hit_dice
+      "#{MAX_DICE_VALUE[hit_dice]} + your Constitution modifier"
     end
   end
 end
--- test/helpers/character_classes_helper_test.rb
+++ test/helpers/character_classes_helper_test.rb
@@ -4,9 +4,13 @@ class CharacterClassesHelperTest < ActionView::TestCase
   include GeneralItemsHelper
   include CharacterClassesHelper
 
-  test 'should format the hit dice' do
-    features = class_features_for(hit_dice: :d12).first
+  test 'should format the hit dice information' do
+    features = class_features_for(hit_dice: :d12)
 
-    assert_equal(['Hit Dice', '1d12 per level'], features)
+    assert_equal(
+      [
+        ['Hit Dice', '1d12 per level'],
+        ['Hit Points at 1st Level', '12 + your Constitution modifier']
+      ], features)
   end
 end
