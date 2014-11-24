71444573b096665be325dc42086d49fb6efec773
Ulisses <anizark@gmail.com>
2014-11-23 23:28:11 -0200

    Add the Acolyte descriptions

--- app/models/background.rb
+++ app/models/background.rb
@@ -12,7 +12,13 @@ class Background
   ACOLYTE = {
     slug: :acolyte,
     name: 'Acolyte',
-    description: '',
+    description: "<p>You have sepen your life in the service of a temple to a "\
+      "specific god or pantheon of gods. You act as an intermediary between "\
+      "the realm of the holy and the mortal world, performing sacred rites "\
+      "and offering sacrifices in order to conduct worshipers into the "\
+      "presence of the the divine. You are not necessarily a cleric - "\
+      "performing sacred rites is not the same thing as channeling divine "\
+      "power.</p>",
     languages_quantity: 2,
     skill_proficiencies: [:insight, :religion],
     equipments: [
@@ -27,7 +33,20 @@ class Background
     },
     feature: {
       name: 'Shelter of the Faithful',
-      description: ''
+      description: "<p>As an acolyte, you command the respect those who share "\
+        "your faith, and you can perform the religious ceremonies of your "\
+        "deity. You and your adventuring companions can expect to receive "\
+        "free healing and care at a temple, shrine, or other established "\
+        "presence of your faith, though you must provide any material "\
+        "components needed for spells. Those who sahre your religion will "\
+        "support you(bunt only you) at a modest lifestyle.</p> "\
+        "<p>You might also have ties to a specific temple dedicated to your "\
+        "deity or pantheon, and you have residence there. This could be the "\
+        "temple where you used to serve, if you remain on good term with it, "\
+        "or a temple where you have found a new home. While near your temple, "\
+        "you can call upon the priests for assistance, provided the assitance "\
+        "you ask for is not hazardous and you remain in good standing with "\
+        "your temple.</p>"
     }
   }
 
