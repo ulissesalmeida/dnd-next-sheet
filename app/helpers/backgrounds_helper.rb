99c71531605c4bd569f422f0cfd0786084ea9ca7
Ulisses <anizark@gmail.com>
2014-11-23 22:22:09 -0200

    Format the tool proficiences for background

--- app/helpers/backgrounds_helper.rb
+++ app/helpers/backgrounds_helper.rb
@@ -2,7 +2,8 @@ module BackgroundsHelper
   def background_traits_for(background)
     [
       label_and_value('Skill Proficiencies', format_items(background[:skill_proficiencies])),
-      label_and_value('Languages', format_choosable_quantity(background[:languages_quantity]))
+      label_and_value('Languages', format_choosable_quantity(background[:languages_quantity])),
+      label_and_value('Tool Proficiencies', format_items(background[:tool_proficiencies]))
     ].select(&:present?)
   end
 
--- test/helpers/backgrounds_helper_test.rb
+++ test/helpers/backgrounds_helper_test.rb
@@ -3,15 +3,21 @@ require 'test_helper'
 class BackgroundsHelperTest < ActionView::TestCase
   include GeneralItemsHelper
 
-  test 'should return the skill proficiencies description for given background' do
+  test 'should return the skill proficiencies description' do
     description = background_traits_for(skill_proficiencies: [:religion, :insight]).first
 
     assert_equal(['Skill Proficiencies', 'Religion and Insight'], description)
   end
 
-  test 'should return the languages choosable quantity' do
+  test 'should return the languages choosable quantity description' do
     description = background_traits_for(languages_quantity: 2).first
 
     assert_equal(['Languages', '2 of your choice'], description)
+  end
+
+  test 'should return the tool proficiencies description' do
+    description = background_traits_for(tool_proficiencies: [:disguise_kit, :one_type_of_musical_instrument]).first
+
+    assert_equal(['Tool Proficiencies', 'Disguise Kit and One Type Of Musical Instrument'], description)
   end
 end
