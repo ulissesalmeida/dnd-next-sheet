0ed6e44f367f437edd7f30a0c4ce5233eed822da
Felipe Mota <sanomota@gmail.com>
2014-11-27 23:32:06 +0000

    Add menu and store user items selection on cookies

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -7,4 +7,18 @@ class RacesController < ApplicationController
     @races = Race.all
     @race = Race.find_by_slug(params[:id])
   end
+
+  def create
+    @race = Race.find_by_slug(params[:id])
+    cookies[:race_name] = @race[:name]
+    cookies[:race_slug] = @race[:slug]
+    cookies.delete :race_variant_name
+    cookies.delete :race_variant_slug
+    cookies.delete :class_name
+    cookies.delete :class_slug
+    cookies.delete :background_name
+    cookies.delete :background_slug
+
+    redirect_to race_path(params[:id])
+  end
 end
