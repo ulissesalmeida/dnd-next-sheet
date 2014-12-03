a1533428bb97b99ff4f25761c884fd4d7cf9f60e
Ulisses <anizark@gmail.com>
2014-12-03 11:23:52 -0200

    Simplify the character steps list
    Renamed the some classes, partials to express character sheet better.
    Use the CharacterModel on views.
    Merge the character steps and menu in one component.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  helper_method :character_sheet
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def character_sheet
+    @character_sheet ||= begin
+      attributes = cookies[:character_sheet] ? JSON.parse(cookies[:character_sheet]) : {}
+      CharacterSheet.new(attributes)
+    end
+  end
 end
